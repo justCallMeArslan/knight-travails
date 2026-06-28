fd04575bc0a96638035e50a9fc8a582378ac47f9
arslan <244682724+justCallMeArslan@users.noreply.github.com>
2026-06-28 21:25:28 +0300

    docs: provide update

--- main.js
+++ main.js
@@ -1,2 +1,5 @@
 // updated theory part.. proceed to project after completing practical exercises 
 // in khanacademy as per theory part of project
+
+// attempted to pass test on khan with minimum thory knowldge (60% score) - need
+// to redo.
