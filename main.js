d91d40d41d04f8ce63288f58e9064102f6c97549
arslan <244682724+justCallMeArslan@users.noreply.github.com>
2026-06-30 11:22:06 +0300

    docs: changed prep status

--- main.js
+++ main.js
@@ -1,5 +1,1 @@
-// updated theory part.. proceed to project after completing practical exercises 
-// in khanacademy as per theory part of project
-
-// attempted to pass test on khan with minimum thory knowldge (60% score) - need
-// to redo.
+// All theory part with practical finished. Ready for the project
